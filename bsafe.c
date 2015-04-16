ee21a6e83920f473673d769e1882d483fb885608
Paul Hsieh <github@azillionmonkeys.com>
2015-04-16 00:04:38 -0700

    Fix documentation bugs.  Use (void) var in bsafe.c for unused variables.

--- bsafe.c
+++ bsafe.c
@@ -24,16 +24,16 @@ char * strcpy (char *dst, const char *src);
 char * strcat (char *dst, const char *src);
 
 char * strcpy (char *dst, const char *src) {
-	dst = dst;
-	src = src;
+	(void) dst;
+	(void) src;
 	fprintf (stderr, "bsafe error: strcpy() is not safe, use bstrcpy instead.\n");
 	if (bsafeShouldExit) exit (-1);
 	return NULL;
 }
 
 char * strcat (char *dst, const char *src) {
-	dst = dst;
-	src = src;
+	(void) dst;
+	(void) src;
 	fprintf (stderr, "bsafe error: strcat() is not safe, use bstrcat instead.\n");
 	if (bsafeShouldExit) exit (-1);
 	return NULL;
@@ -41,7 +41,7 @@ char * strcat (char *dst, const char *src) {
 
 #if !defined (__GNUC__) && (!defined(_MSC_VER) || (_MSC_VER <= 1310))
 char * (gets) (char * buf) {
-	buf = buf;
+	(void) buf;
 	fprintf (stderr, "bsafe error: gets() is not safe, use bgets.\n");
 	if (bsafeShouldExit) exit (-1);
 	return NULL;
@@ -49,33 +49,33 @@ char * (gets) (char * buf) {
 #endif
 
 char * (strncpy) (char *dst, const char *src, size_t n) {
-	dst = dst;
-	src = src;
-	n = n;
+	(void) dst;
+	(void) src;
+	(void) n;
 	fprintf (stderr, "bsafe error: strncpy() is not safe, use bmidstr instead.\n");
 	if (bsafeShouldExit) exit (-1);
 	return NULL;
 }
 
 char * (strncat) (char *dst, const char *src, size_t n) {
-	dst = dst;
-	src = src;
-	n = n;
+	(void) dst;
+	(void) src;
+	(void) n;
 	fprintf (stderr, "bsafe error: strncat() is not safe, use bstrcat then btrunc\n\tor cstr2tbstr, btrunc then bstrcat instead.\n");
 	if (bsafeShouldExit) exit (-1);
 	return NULL;
 }
 
 char * (strtok) (char *s1, const char *s2) {
-	s1 = s1;
-	s2 = s2;
+	(void) s1;
+	(void) s2;
 	fprintf (stderr, "bsafe error: strtok() is not safe, use bsplit or bsplits instead.\n");
 	if (bsafeShouldExit) exit (-1);
 	return NULL;
 }
 
 char * (strdup) (const char *s) {
-	s = s;
+	(void) s;
 	fprintf (stderr, "bsafe error: strdup() is not safe, use bstrcpy.\n");
 	if (bsafeShouldExit) exit (-1);
 	return NULL;
